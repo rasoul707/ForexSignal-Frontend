80991c6a0eb2efd90af308da26a42c1677f80918
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-02 19:16:28 +0430

    ws change url

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,6 +1,6 @@
-const baseUrl = 'ws://195.248.242.210:8000/ws/fx/';
+const baseUrl = 'ws://195.248.242.210:8000/ws/';
 
-export const wsSignals = new WebSocket(baseUrl + "signalsAlert/");
+export const wsSignals = new WebSocket(baseUrl + "signals/");
 export const wsArticles = new WebSocket(baseUrl + "articles/");
 
 
