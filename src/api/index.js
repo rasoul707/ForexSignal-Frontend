b1d4ebb1db77c78c85e80979f474e9e267af02ad
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-02 17:44:35 +0430

    change api ip

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://127.0.0.1:8000/api/';
+const baseUrl = 'http://195.248.242.210:8000/api/';
 const headers = () => { return { headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` } } }
 
 
