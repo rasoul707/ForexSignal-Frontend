9c759cc0be908fbf028f6d58c76c869f3c068297
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-02 18:56:22 +0430

    add fx

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,4 +1,4 @@
-const baseUrl = 'ws://195.248.242.210:8000/ws/';
+const baseUrl = 'ws://195.248.242.210:8000/ws/fx/';
 
 export const wsSignals = new WebSocket(baseUrl + "signalsAlert/");
 export const wsArticles = new WebSocket(baseUrl + "articles/");
