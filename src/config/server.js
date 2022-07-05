3580efba105c4c69fdfebce1bc4410a49d3622bd
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-05 11:27:21 +0430

    socket url

--- src/config/server.js
+++ src/config/server.js
@@ -2,6 +2,6 @@
 
 
 export const APIBaseUrl = 'https://therealrole.com:2053/api/';
-export const WSBaseUrl = 'ws://195.248.242.210:2053/ws/';
+export const WSBaseUrl = 'wss://therealrole.com:2053/ws/';
 
 
