6370749bd80ca9e03d8135f9ba9e8e2744daafcd
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-07 20:59:32 +0430

    add circlar prog

--- src/scenes/panel/Help/index.js
+++ src/scenes/panel/Help/index.js
@@ -2,17 +2,19 @@ import * as React from "react"
 
 import Layout from "../../../components/Layout"
 
+import { CircularProgress, } from '@mui/material';
 
 
 const Page = () => {
 
     return <Layout >
+        <CircularProgress />
         <iframe
             title="help"
             src="/chat.html"
             style={{
                 width: "100%",
-                height: "calc(100vh - 64px - 56px - 24px)",
+                height: "calc(100vh - 64px - 56px - 52px)",
                 border: "none",
             }} />
     </Layout>
