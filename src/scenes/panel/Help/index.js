8fb043a5aa55a8ac36ddb270603202c6aab6afe6
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-07 20:47:03 +0430

    add chat to help

--- src/scenes/panel/Help/index.js
+++ src/scenes/panel/Help/index.js
@@ -1,34 +1,20 @@
 import * as React from "react"
 
 import Layout from "../../../components/Layout"
-import { Grid, } from '@mui/material';
-import NewTicket from "./NewTicket"
-import TicketsList from "./TicketsList"
-import { LiveChatWidget, EventHandlerPayload } from "@livechat/widget-react";
+
+
 
 const Page = () => {
 
-    const layout = React.useRef(null)
-
-    const crisp = () => {
-        //         <iframe src="/chat.html" style="
-        //     width: 100%;
-        //     height: calc(100vh - 64px - 56px - 24px);
-        //     border: none;
-        // "></iframe>
-
-    }
-
-    React.useEffect(() => {
-        crisp()
-    })
-
     return <Layout >
-        <LiveChatWidget
-            license="14293815"
-            visibility="maximized"
-        // onNewEvent={handleNewEvent}
-        />
+        <iframe
+            title="help"
+            src="/chat.html"
+            style={{
+                width: "100%",
+                height: "calc(100vh - 64px - 56px - 24px)",
+                border: "none",
+            }} />
     </Layout>
 }
 export default Page
