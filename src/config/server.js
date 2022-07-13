623c98171aa473656d440718280b8eb2e01ea0be
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-13 17:40:23 +0430

    audio error

--- src/config/server.js
+++ src/config/server.js
@@ -1,8 +1,8 @@
 
 
 
-// export const APIBaseUrl = 'https://therealrole.com:2053/api/';
-export const APIBaseUrl = 'http://195.248.242.210:2053/api/';
+export const APIBaseUrl = 'https://therealrole.com:2053/api/';
+// export const APIBaseUrl = 'http://195.248.242.210:2053/api/';
 export const WSBaseUrl = 'wss://therealrole.com:2053/ws/';
 
 
