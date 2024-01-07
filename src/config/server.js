b32d2af9dc443f2ce3a6edc7134ef1b9239eda21
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-01-07 13:26:34 +0330

    update config

--- src/config/server.js
+++ src/config/server.js
@@ -1,8 +1,8 @@
 
 
 
-export const APIBaseUrl = 'https://therealrole.com:2053/api/';
+export const APIBaseUrl = 'https://forexapi.liara.run/api/';
 // export const APIBaseUrl = 'http://195.248.242.210:2053/api/';
-export const WSBaseUrl = 'wss://therealrole.com:2053/ws/';
+export const WSBaseUrl = 'wss://forexapi.liara.run/ws/';
 
 
